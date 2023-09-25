f0d967b5a218ca342cef34dcdbb67853635ec783
NikolozFilishvili <125210992+NikolozFilishvili@users.noreply.github.com>
2023-09-25 22:38:00 +0400

    Add files via upload

---
+++ lesson 2.2.py
@@ -0,0 +1,8 @@
+string1= "I am"
+name="Nikoloz FiliShvili"
+string2="I am 15 years old"
+string3="my height is above 180cm"
+string4="I was born in 2008"
+string5=" I live in Georgia,Rustavi"
+
+print(string1+ " "+name+" "+string2+" "+string3+" "+string4+" "+string5)
